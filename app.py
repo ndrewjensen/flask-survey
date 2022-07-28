34cb8fa467179b8a0275814fecf3a5c33ace6c2e
Julie Han <juliemenghan@gmail.com>
2022-07-28 19:28:22 -0400

    step 1-6 done and reviewed

--- app.py
+++ app.py
@@ -10,34 +10,50 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
-question_idx =0
+question_idx = 0
 
 @app.get('/')
 def render_initial_page():
+    """Displays the survey start page"""
     
     return render_template(
         '/survey_start.html',
-        survey_title = survey.title,
-        survey_instructions = survey.instructions)
+        survey = survey)
     
 @app.post('/begin')
 def redirect_to_first_question():
+    """Display survey question and choices"""
 
     return redirect('/question/0')
 
-@app.get('/question/<int:question_idx>')
+@app.get("/question/<int:question_idx>")
 def display_question(question_idx):
     
     question = survey.questions[question_idx]
-    choices = question.choices
 
     return render_template(
         '/question.html',
-        question = question,
-        choices = choices)
+        question = question)
 
 
-# @app.post('/answer')
-# def take_answer_and_redirect():
-#     request.
+@app.post('/answer')
+def take_answer_and_redirect():
+    """Gets user answer, updates the answers list 
+    and redirects the user to the next questions or shows them the completion page
+    if the survey is finished"""
+    
+    answer = request.form["answer"]
+    responses.append(answer)
+    question_idx = len(responses)
 
+    
+    if question_idx == len(survey.questions):  
+        return redirect("/completion")
+
+    return redirect(f"/question/{question_idx}")
+
+@app.get("/completion")
+def thank_you():
+    """Displays the completion page, and thanks the user"""
+
+    return render_template("/completion.html")
